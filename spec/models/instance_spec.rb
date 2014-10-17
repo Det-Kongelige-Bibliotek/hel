d847c9f855f1a3235149fdfbd52ddba3ca2afab1
Ronan McHugh <mchugh.r@gmail.com>
2014-10-17 14:15:27 +0200

    UUID methods for Instance.

--- spec/models/instance_spec.rb
+++ spec/models/instance_spec.rb
@@ -5,4 +5,11 @@ describe Instance do
     i = Instance.new
     expect(i.content_files.size).to eql 0
   end
+
+  it 'should have a uuid on creation' do
+    i = Instance.new
+    expect(i.uuid).to be_nil
+    i.save
+    expect(i.uuid.present?).to be true
+  end
 end
