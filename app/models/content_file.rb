77215d306d76064a76f0077a9bcf8cd50e0f55c0
Ronan McHugh <mchugh.r@gmail.com>
2015-04-10 13:49:29 +0200

    Add note about absolute paths.

--- app/models/content_file.rb
+++ app/models/content_file.rb
@@ -34,7 +34,7 @@ class ContentFile < ActiveFedora::Base
   end
 
   # Adds a content datastream to the object as an external managed file in fedore
-  #
+  # Note that this should be an absolute path!
   # @param path external url to the file
   def add_external_file(path)
     file_name = Pathname.new(path).basename.to_s
