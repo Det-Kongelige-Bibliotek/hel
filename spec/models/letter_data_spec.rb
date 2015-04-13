985a8fcbe11a166d86c2e175d2413773dd0751a7
Ronan McHugh <mchugh.r@gmail.com>
2015-04-13 15:36:27 +0200

    test for LetterData

---
+++ spec/models/letter_data_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe 'LetterData' do
+
+  before :each do
+    f = File.new(Rails.root.join('spec', 'fixtures', 'breve', '001541111_000', '001541111_000.xml'))
+    xml = Nokogiri::XML(f.read())
+    div = xml.css('text body div').first
+    @data = LetterVolumeSplitter::LetterData.new(div)
+  end
+
+  describe 'title' do
+
+    it 'should build a title' do
+      expect(@data.title).to include 'Tove'
+    end
+
+    it 'should parse the sender' do
+      expect(@data.sender_name).to eql 'Victor'
+    end
+  end
+end
