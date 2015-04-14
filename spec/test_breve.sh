45a1b978b97aa3079cae94e5109627558e5e2db8
Ronan McHugh <mchugh.r@gmail.com>
2015-04-14 10:53:24 +0200

    don't follow the log automatically

--- spec/test_breve.sh
+++ spec/test_breve.sh
@@ -6,5 +6,4 @@ set -o errexit
 rake adl:clean
 rake breve:create_activity
 rake breve:import_from_path['spec/fixtures/breve/001541111_000']
-rake environment resque:work RAILS_ENV=development QUEUE=* &
-tail -f log/resque.log
+rake environment resque:work RAILS_ENV=development QUEUE=*
