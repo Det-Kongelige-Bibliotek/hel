28bc2dc2327d2ad2a6064f68db0c832d8f9f310c
Ronan McHugh <mchugh.r@gmail.com>
2015-04-22 14:35:11 +0200

    no inverse_of in belongs_to

--- app/models/title.rb
+++ app/models/title.rb
@@ -1,5 +1,5 @@
 class Title < ActiveFedora::Base
-  belongs_to :work, predicate: ::RDF::Vocab::Bibframe.relatedTo, inverse_of: ::RDF::Vocab::Bibframe.title
+  belongs_to :work, predicate: ::RDF::Vocab::Bibframe.relatedTo
   property :value, predicate: ::RDF::Vocab::Bibframe.titleValue, multiple: false
   property :variant, predicate: ::RDF::Vocab::Bibframe.titleType, multiple: false
   property :subtitle, predicate: ::RDF::Vocab::Bibframe.subtitle, multiple: false
