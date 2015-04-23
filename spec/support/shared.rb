8c9348fcd2c45095ed8c2c3283e0f4acca6b3a85
Ronan McHugh <mchugh.r@gmail.com>
2015-04-23 10:19:49 +0200

    add shared work method to DRY things up.

--- spec/support/shared.rb
+++ spec/support/shared.rb
@@ -4,9 +4,23 @@
 # e.g.
 # include_context 'shared'
 # before :each do
-#   @instance = Instance.new(instance_params)
+#   instance = Instance.new(instance_params)
 # end
 RSpec.shared_context 'shared' do
   let (:instance_params) { { collection: 'Sample', activity: Administration::Activity.create(activity: 'test').id, copyright: 'cc' }}
   let (:valid_trykforlaeg) { instance_params.merge(isbn13: '9780521169004', published_date: '2004')}
+  before do
+    agent = Authority::Person.create(
+        'authorized_personal_name' => { 'given'=> 'Proinsias', 'family' => 'De Rossa', 'scheme' => 'KB', 'date' => '1932/2009' }
+    )
+    @valid_work = Work.new
+    @valid_work.add_title({'value'=> 'A title'})
+    @valid_work.add_author(agent)
+    @valid_work.save # for these tests to work. Object has to be persisted. Otherwise relations cannot be updated
+    rel = Work.new
+    rel.add_title({'value'=> 'A title'})
+    rel.add_author(agent)
+    rel.save # for
+    @valid_work
+  end
 end
