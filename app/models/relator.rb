38176d57976988945a907ee92e6db7049972f3d2
Ronan McHugh <mchugh.r@gmail.com>
2015-04-27 16:19:42 +0200

    Add agent_id method so we can set relation via an id only

--- app/models/relator.rb
+++ app/models/relator.rb
@@ -3,13 +3,24 @@ class Relator < ActiveFedora::Base
   belongs_to :agent, predicate: ::RDF::Vocab::Bibframe.agent, class_name: 'ActiveFedora::Base'
   property :relator_role, predicate: ::RDF::Vocab::Bibframe.relatorRole, multiple: false
 
+  validates :work, :agent, :relator_role, presence: true
+
+  # we show the uri
   def role
-    self.relator_role
+    self.relator_role.to_term.value
   end
 
   # When the value being set is a rdf:resource,
   # we need to wrap the setter as follows
   def role=(uri)
     self.relator_role = ::RDF::URI.new(uri)
+  end
+
+  def agent_id=(id)
+    self.agent = ActiveFedora::Base.find(id)
+  end
+
+  def agent_id
+    self.agent.try(:id)
   end
 end
