90f548ac93e448822f38480dff57320df2c7619a
Ronan McHugh <mchugh.r@gmail.com>
2015-04-27 16:04:36 +0200

    Add finder method.

--- app/models/administration/list_entry.rb
+++ app/models/administration/list_entry.rb
@@ -5,5 +5,11 @@ module Administration
     attribute :label
     index :name
     reference :controlled_list, Administration::ControlledList
+
+    # Given the name of an entry - find it and get the corresponding label
+    # e.g. Administration::ListEntry.get_label('http://id.loc.gov/vocabulary/relators/abr') => 'Abridger'
+    def self.get_label(name)
+      self.find(name: name).first.label rescue nil
+    end
   end
 end
