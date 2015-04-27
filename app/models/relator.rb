809c828cda00e964eeb4088288b4c6c7ce77ec25
Ronan McHugh <mchugh.r@gmail.com>
2015-04-27 09:33:21 +0200

    rdf resource setter for relator role

--- app/models/relator.rb
+++ app/models/relator.rb
@@ -2,4 +2,14 @@ class Relator < ActiveFedora::Base
   belongs_to :work, predicate: ::RDF::Vocab::Bibframe.relatedTo
   belongs_to :agent, predicate: ::RDF::Vocab::Bibframe.agent, class_name: 'ActiveFedora::Base'
   property :relator_role, predicate: ::RDF::Vocab::Bibframe.relatorRole, multiple: false
+
+  def role
+    self.relator_role
+  end
+
+  # When the value being set is a rdf:resource,
+  # we need to wrap the setter as follows
+  def role=(uri)
+    self.relator_role = ::RDF::URI.new(uri)
+  end
 end
