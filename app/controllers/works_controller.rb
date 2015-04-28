73ee2ac33d0ffd0ead7112aa69004ae09ad8bfe1
Ronan McHugh <mchugh.r@gmail.com>
2015-04-28 09:29:55 +0200

    allow id through to prevent duplicate relators being created.

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -125,7 +125,7 @@ class WorksController < ApplicationController
   # Never trust parameters from the scary internet, only allow the white list through.
   def work_params
     params[:work].permit(:language, titles_attributes: [[:id, :value, :subtitle, :lang, :type]],
-                         relators_attributes: [[ :agent_id, :role ]], subjects: [[:id]], note:[]).tap do |fields|
+                         relators_attributes: [[ :id, :agent_id, :role ]], subjects: [[:id]], note:[]).tap do |fields|
       # remove any inputs with blank values
       fields['titles_attributes'] = fields['titles_attributes'].select {|k,v| v['value'].present? }
     end
