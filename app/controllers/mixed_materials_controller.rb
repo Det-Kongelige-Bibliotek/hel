89a2793554e44bec001ad2a0b6a6c4893c1b8cd3
Ronan McHugh <mchugh.r@gmail.com>
2015-06-15 11:30:35 +0200

    first draft of mixed material form.

--- app/controllers/mixed_materials_controller.rb
+++ app/controllers/mixed_materials_controller.rb
@@ -2,5 +2,9 @@ class MixedMaterialsController < ApplicationController
   authorize_resource
   def new
     @mixed_material = MixedMaterial.new
+    @mixed_material.titles.build
+    @mixed_material.relators.build
+    @mixed_material.instances.build
+    @instance = @mixed_material.instances.first
   end
 end
