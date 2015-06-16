9ce0cf322d6628e7b4896ace89afa358d5d5d382
Ronan McHugh <mchugh.r@gmail.com>
2015-06-16 10:57:10 +0200

    show params instead of timestamp in dev mode.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,10 @@ Rails.application.configure do
   config.lograge.enabled = true
   # add time to lograge
   config.lograge.custom_options = lambda do |event|
-    {:time => event.time}
+    params = event.payload[:params].reject do |k|
+      ['controller', 'action'].include? k
+    end
+    { "params" => params }
   end
 
   # Raises error for missing translations
