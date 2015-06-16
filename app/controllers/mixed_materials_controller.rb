31c7abdd75d1c70acaa83cff0c5114aab0427482
Ronan McHugh <mchugh.r@gmail.com>
2015-06-16 15:59:42 +0200

    Functioning file upload.

--- app/controllers/mixed_materials_controller.rb
+++ app/controllers/mixed_materials_controller.rb
@@ -44,11 +44,11 @@ class MixedMaterialsController < ApplicationController
   def mixed_material_params
     params[:mixed_material].permit(:language, :origin_date, titles_attributes: [[:id, :value, :subtitle, :lang, :type]],
                          relators_attributes: [[ :id, :agent_id, :role ]], subjects: [[:id]], note:[],
-                         instances_attributes: [[ :id, :type, :activity, :title_statement, :extent, :copyright,
+                         instances_attributes: [ [ :id, :type, :activity, :title_statement, :extent, :copyright,
                          :dimensions, :mode_of_issuance, :isbn13, :contents_note, :embargo,
                          :embargo_date, :embargo_condition, :access_condition, :availability,
-                         :collection, :preservation_profile, note: [], content_files: [],
+                         :collection, :preservation_profile, note: [],
                          relators_attributes: [[ :id, :agent_id, :role ]],
-                         publications_attributes: [[:id, :copyright_date, :provider_date ]] ]])
+                         publications_attributes: [[:id, :copyright_date, :provider_date ]] ]], content_files: [])
   end
 end
