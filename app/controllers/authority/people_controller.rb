826f7ef9da572f7b9c22d65e8ac08ee040db08c7
Polyxeni Makri <poma@kb.dk>
2015-06-29 16:22:19 +0200

    Check if there is a givenName in viaf, if not an alert box appears

--- app/controllers/authority/people_controller.rb
+++ app/controllers/authority/people_controller.rb
@@ -14,18 +14,19 @@ module Authority
       reader = RDF::Reader.open(params[:url])
       stats = reader.each_statement.to_a
 
-      unless stats.empty?
+      if stats.select {|s| s.predicate == 'http://schema.org/givenName' }.empty?
+        json_file ={}
+      else
         first_name = stats.select {|s| s.predicate == 'http://schema.org/givenName' }.first.object.value
-        family_name = stats.select {|s| s.predicate == 'http://schema.org/familyName' }.first.object.value
-        alternate_name = stats.select {|s| s.predicate == 'http://schema.org/alternateName' }.first.object.value
-        isni_uri = stats.select {|s| s.predicate == 'http://schema.org/sameAs' and
-                                    s.object.value.include? 'http://isni.org/isni/'}.first.object.value
+        family_name = stats.select {|s| s if s.predicate == 'http://schema.org/familyName' }.first.object.value
       end
+      alternate_name = stats.select {|s| s.predicate == 'http://schema.org/alternateName' }.first.object.value
+      isni_uri = stats.map {|s| s if s.predicate == 'http://schema.org/sameAs' and
+                                      s.object.value.include? 'http://isni.org/isni/'}.compact.first.object.value
+
 
       json_file = {:first_name => first_name, :family_name => family_name, :alternate_name => alternate_name,
                     :isni_uri => isni_uri}
-
-      logger.info json_file
       render json: json_file.to_json
     end
 
