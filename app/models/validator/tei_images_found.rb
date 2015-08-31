fd2f5144c890c3c28cf26fe1f9b2feb7dd428262
David Grove Jørgensen <dgj@kb.dk>
2015-08-31 15:37:00 +0200

    updated solr search

--- app/models/validator/tei_images_found.rb
+++ app/models/validator/tei_images_found.rb
@@ -7,7 +7,7 @@ module Validator
         record.content_files.each do |cf|
           xdoc = Nokogiri::XML.parse(cf.content) { |config| config.strict }
           xdoc.xpath("//xmlns:pb").each do |n|
-            res = ActiveFedora::SolrService.query("pb_xml_id_tesim:#{n.attr('xml:id')} && pb_facs_id_tesim:#{n.attr('facs')}")
+            res = ActiveFedora::SolrService.query("pb_xml_id_tesim:#{n.attr('xml:id')} && pb_facs_id_si:#{n.attr('facs')}")
 
             record.errors[:base] << "No image found for #{n.to_s}" if (res.size == 0)
             record.errors[:base] << "More than 1 image found for #{n.to_s}" if (res.size > 1)
