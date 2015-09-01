1cbdefe5edbc27af89c36406ab7322b3f260c725
David Grove Jørgensen <dgj@kb.dk>
2015-09-01 15:50:39 +0200

    search for facs

--- app/models/validator/tei_images_found.rb
+++ app/models/validator/tei_images_found.rb
@@ -7,7 +7,7 @@ module Validator
         record.content_files.each do |cf|
           xdoc = Nokogiri::XML.parse(cf.content) { |config| config.strict }
           xdoc.xpath("//xmlns:pb").each do |n|
-            res = ContentFile.find_by_pb_facs_id(n.attr('xml:id'))
+            res = ContentFile.find_by_pb_facs_id(n.attr('facs'))
             record.errors[:base] << "No image found for #{n.to_s}" if (res.size == 0)
             record.errors[:base] << "More than 1 image found for #{n.to_s}" if (res.size > 1)
 
