35034e54754cbef4d4cbd688b28483ac31d8e39d
Nader <nkh@kb.dk>
2015-10-29 11:08:01 +0100

    New Unit test for Authority->PeopleController

--- spec/controllers/authority/bases_controller_spec.rb
+++ spec/controllers/authority/bases_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe Authority::BasesController, :type => :controller do
-
+  describe 'new' do
+    it 'create new authority object' do
+      authority = Authority::Bases.new
+      expect(authority).to be_a Authority
+    end
+  end
 end
