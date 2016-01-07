9f91c7d95a40c78866c51cc0c7c31df2e05e5c79
Polyxeni Makri <poma@kb.dk>
2016-01-07 16:08:54 +0100

    Create edit and show page that updates the work parameters

---
+++ app/controllers/letter_books_controller.rb
@@ -0,0 +1,36 @@
+class LetterBooksController < ApplicationController
+  include Concerns::RemoveBlanks
+  before_action :set_letter_book, only: [:show, :edit, :update]
+
+  respond_to :html
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @letter_book.update_work(work_params)
+      flash[:notice] =  t(:model_updated, model: t('models.letter_book'))
+    end
+    respond_with @letter_book
+  end
+
+  private
+
+  def set_letter_book
+    @letter_book = LetterBook.find(URI.unescape(params[:id]))
+  end
+
+  def work_params
+    params[:letter_book].permit( :origin_date, titles_attributes: [[:id, :value, :subtitle, :lang, :type]])
+  end
+
+  def instance_params
+
+  end
+
+end
