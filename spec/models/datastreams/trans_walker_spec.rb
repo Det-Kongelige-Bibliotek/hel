1bfbbc1f54feb096adda26b9ab5e532a0066ee4d
Sigfrid Lundberg <siggelundberg@gmail.com>
2016-01-07 15:06:37 +0100

    added tests for initializing a letter_book from aleph

--- spec/models/datastreams/trans_walker_spec.rb
+++ spec/models/datastreams/trans_walker_spec.rb
@@ -3,11 +3,13 @@ require 'spec_helper'
 
 describe 'trans walker' do
   # let(:id) { '9788711396322' } # Min kamp
-  let(:id) { '9780691129785' } # The origin
-  let(:type) {'isbn'}
+#  let(:id_val) { '9780691129785' } # The origin
+  let(:id_val) {'001003523'} # Ditlevsen's letters
+#  let(:id_type) {'isbn'}
+  let(:id_type) {'sys'}
 
   describe 'Work.from_mods' do
-    let(:work) { ConversionService.work_from_aleph(type, id)}
+    let(:work) { ConversionService.work_from_aleph(id_type, id_val)}
 
     it 'should initialize a work from mods ' do
       expect(work).to be_a Work
@@ -27,10 +29,10 @@ describe 'trans walker' do
   end
 
   describe 'Instance.from_mods' do
-    let(:instance) { ConversionService.instance_from_aleph('isbn', '9788711396322' )}
+    let(:instance) { ConversionService.instance_from_aleph(id_type,id_val)}
 
     it 'should initialize an instance from mods ' do
-      expect(instance.isbn13).to eql isbn
+      expect(instance.publisher.first.present?).to eql true
     end
   end
 
--- spec/models/letter_book_spec.rb
+++ spec/models/letter_book_spec.rb
@@ -1,7 +1,13 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
+
+#let(:aleph_value) {'001003523'} #Brandes
+#let(:aleph_value) {'001541111'} # Ditlevsen's letters
 
 describe LetterBook do
   include_context 'shared'
+  let(:aleph_value) {'001541111'} # Ditlevsen's letters
+  let(:aleph_field) {'sys'}
 
   it "can create a new Letter Book" do
    lb = LetterBook.new_letterbook(work_params,instance_params)
@@ -29,5 +35,31 @@ describe LetterBook do
     tiff = lb.get_instance("TIFF")
     expect(tiff.preservation_collection).to eql "storage"
   end
+
+  it "can update work attributes from aleph" do
+    mods = ConversionService.aleph_to_mods(aleph_field, aleph_value)
+    lb = LetterBook.new_letterbook({},{})
+    lb.save
+    @service = AlephService.new
+    set = @service.find_set("sys="+aleph_value) 
+    rec = @service.get_record(set[:set_num],set[:num_entries])
+    puts rec
+    puts mods.to_xml()
+    lb.from_mods(mods)
+    expect(lb.title_values.first).to include 'Kære Victor'
+  end
+
+  it "can update instance attributes from aleph" do
+    mods = ConversionService.aleph_to_mods(aleph_field, aleph_value)
+    lb = LetterBook.new_letterbook({},{})
+    lb.save
+    tei = lb.get_instance("TEI")
+    # tiff = lb.get_instance("TIFF")
+    puts mods.to_xml()
+    tei.from_mods(mods)
+    # tiff.from_mods(mods) and an instance is an instance is an instance
+    expect(tei.publisher_name).to include 'Gyldendal'
+  end
+
 
 end
