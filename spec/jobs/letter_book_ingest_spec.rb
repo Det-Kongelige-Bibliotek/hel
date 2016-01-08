6843d9575d3f30a5c2bbeab2581db4f72928640e
Sigfrid Lundberg <siggelundberg@gmail.com>
2016-01-08 15:14:05 +0100

    finding ways to debug

--- spec/jobs/letter_book_ingest_spec.rb
+++ spec/jobs/letter_book_ingest_spec.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 require 'fakeredis'
 require 'resque'
@@ -5,9 +6,39 @@ require 'resque'
 describe 'ingest a letter_book' do
   include_context 'shared'
 
+  before :all do
+
+    activity = Administration::Activity.create
+    (
+     {
+       activity: 'Danmarks Breve',
+       embargo:  '1',
+       access_condition:'',
+       copyright: 'CC BY-NC-ND',
+       preservation_collection: 'storage',
+       availability: '',
+       collection: ["dasam3"],
+       activity_permissions:
+       {
+         "file"=>{
+           "group"=>{"discover"=>["Chronos-Alle"],
+             "read"=>["Chronos-Alle"],
+             "edit"=>["Chronos-Alle"] }
+         },
+         "instance"=>{
+           "group"=>{"discover"=>["Chronos-Alle"],
+             "read"=>["Chronos-NSA","Chronos-Admin"],
+             "edit"=>["Chronos-NSA","Chronos-Admin"]
+           }
+         }
+       }
+     }
+     )
+  end
+
   describe 'tei file' do
     it 'it should read an xml file' do 
-      LetterBookIngest.perfom("spec/fixtures/breve/001003523_000/001003523_000.xml")
+      LetterBookIngest.perform("spec/fixtures/breve/001003523_000/001003523_000.xml")
     end
   end
 
