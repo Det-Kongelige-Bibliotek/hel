4dce5edd8ca00486e86120588b374e38b03decfc
Sigfrid Lundberg <siggelundberg@gmail.com>
2016-01-08 10:53:41 +0100

    making a test for the loading of letter_book tei files

---
+++ spec/jobs/letter_book_ingest_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require 'fakeredis'
+require 'resque'
+
+describe 'loading letter_book tei files with resque' do
+  include_context 'shared'
+  describe 'of a content file' do
+  end
+end
+
