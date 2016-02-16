17779be71816b83ac9de1c985fdbb55df413c62a
David Grove Jørgensen <dgj@kb.dk>
2016-02-16 10:25:06 +0100

    Now handling errors and redirecting back to letters page

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -1,13 +1,21 @@
 class LettersController < ApplicationController
   def update
     # here we do the letter update
-    json = letter_params
-    parts = json[:file].rpartition("/")
-    coll = "/db#{ parts.first }"
-    SnippetServer.update_letter(parts.last,json[:id],json.to_json.html_safe,{:c => coll, :op=>'solrize'})
-    render text: json.to_json.html_safe
-  end
+    begin
+      json = letter_params
+      parts = json[:file].rpartition("/")
+      coll = "/db#{ parts.first }"
+      res = SnippetServer.update_letter(parts.last,json[:id],json.to_json.html_safe,{:c => coll, :op=>'solrize'})
+      flash[:notice] = 'Brev Opdateret'
+    rescue Exception => e
+      logger.error e.message
+      logger.error e.backtrace.join("\n")
+      flash[:error] = "Kunne ikke opdatere brev. Proplemer med server"
+    end
+    letter_id = "#{File.dirname(json[:file])}/#{File.basename(json[:file],'.xml')}-#{json[:id]}"
 
+    redirect_to "#{solr_document_path(letter_id)}"
+  end
 
 
   private
@@ -28,7 +36,7 @@ class LettersController < ApplicationController
       phash = {}
       person_object = Authority::Person.where(id: p['auth_id']).first
       if person_object.present?
-        phash['auth_id'] = CONFIG[Rails.env.to_sym][:application_url]+"/authority/people/"+URI.encode(p['auth_id'])
+        phash['auth_id'] = p['auth_id']
         phash['xml_id'] = p['xml_id']
         phash['given_name'] = person_object.given_name
         phash['family_name'] = person_object.family_name
