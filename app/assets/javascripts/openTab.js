19901ce378927e9eb303ccd8a88dbeb2c429f3c4
Nader <nkh@kb.dk>
2016-02-24 09:00:57 +0100

    Shows whole facsimile

--- app/assets/javascripts/openTab.js
+++ app/assets/javascripts/openTab.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     var myid = window.location.hash;
-    //alert (myid);
     if (myid) {
         (myid.indexOf("facsid") > -1)? $('#facs').addClass('active') : $('#text').addClass('active');
         $(myid)[0].scrollIntoView();
--- spec/models/snippet_server_spec.rb
+++ spec/models/snippet_server_spec.rb
@@ -9,7 +9,7 @@ describe 'Snippet Server for letters' do
   describe 'render html' do
     it 'return an html page' do
       opts[:op] = 'render'
-      html = SnippetServer.render_snippet(id, opts)
+      html = SnippetServer.render_snippet(id, opts, 1)
       expect(html).to start_with '<div class'
       expect(html).to end_with '</div>'
     end
@@ -18,7 +18,7 @@ describe 'Snippet Server for letters' do
   describe 'solrize' do
     it 'return a solr document with one work/letter' do
       opts[:op] = 'solrize'
-      solr_doc = SnippetServer.render_snippet(id, opts)
+      solr_doc = SnippetServer.render_snippet(id, opts, 1)
       count_trunk = solr_doc.scan(/>trunk</).count
       expect(solr_doc).to start_with '<add>'
       expect(solr_doc).to end_with '</add>'
