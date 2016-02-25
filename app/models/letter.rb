cd0d6d004bf0cfe1b32aac9d8a4143b3f249b584
David Grove Jørgensen <dgj@kb.dk>
2016-02-25 09:49:51 +0100

    updated letter modes

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -1,6 +1,6 @@
 class Letter
   include ActiveModel::Model
-  attr_accessor :id, :file, :sender, :recipient, :place, :date
+  attr_accessor :id, :work_id, :xml_id, :file, :sender, :recipient, :place, :date
 
   def persisted?
     true
@@ -8,6 +8,6 @@ class Letter
 
   def self.from_json(data)
      json = JSON.parse(data)
-     Letter.new(id: json["id"], file: json['file'], sender: json["sender"], recipient: json["recipient"], place: json['place'], date: json['date'])
+     Letter.new(id: json["id"], xml_id: json['xml_id'], file: json['file'], sender: json["sender"], recipient: json["recipient"], place: json['place'], date: json['date'])
   end
 end
