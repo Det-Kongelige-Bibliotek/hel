34b079cee4bf272f869db8c2693160e08a246388
David Grove Jørgensen <dgj@kb.dk>
2016-03-02 14:43:51 +0100

    fixed queue name

--- app/jobs/letter_book_scan.rb
+++ app/jobs/letter_book_scan.rb
@@ -1,6 +1,6 @@
 class LetterBookScan
 
-  @queue = :letter_book_ingest
+  @queue = :letter_book_scan
 
   def self.perform(incomming_path,processed_path,img_base_path)
     Dir.glob("#{incomming_path}/*.xml").each do |xml_path|
