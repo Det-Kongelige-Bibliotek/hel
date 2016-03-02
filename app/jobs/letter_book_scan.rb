9df3359b038b66c4138d127862b97088e23d5606
David Grove Jørgensen <dgj@kb.dk>
2016-03-02 14:12:10 +0100

    added resque job to scan for new letterbook files

---
+++ app/jobs/letter_book_scan.rb
@@ -0,0 +1,12 @@
+class LetterBookScan
+
+  @queue = :letter_book_ingest
+
+  def self.perform(incomming_path,processed_path,img_base_path)
+    Dir.glob("#{incomming_path}/*.xml").each do |xml_path|
+      basename = File.basename(xml_path).to_s
+      sysnum = basename.split('_')[0]
+      Resque.enqueue(LetterBookIngest,xml_path,"#{img_base_path}/#{sysnum}","#{processed_path}/#{basename}")
+    end
+  end
+end
