680e281924fa0f2ab5a854a1b269d09d5b78b9ec
jolf@kb.dk <jolf@kb.dk>
2016-03-03 16:10:41 +0100

    fix bug in letter book scan

--- app/jobs/letter_book_scan.rb
+++ app/jobs/letter_book_scan.rb
@@ -5,8 +5,8 @@ class LetterBookScan
   def self.perform(incomming_path,processed_path,img_base_path)
     Dir.glob("#{incomming_path}/*.xml").each do |xml_path|
       basename = File.basename(xml_path).to_s
-      sysnum = basename.split('_')[0]
-      Resque.enqueue(LetterBookIngest,xml_path,"#{img_base_path}/#{sysnum}","#{processed_path}/#{basename}")
+      sysnum = basename.to_s.split('_')[0]
+      Resque.enqueue(LetterBookIngest,xml_path,"#{img_base_path}/#{File.basename(xml_path,'.xml')}","#{processed_path}/#{basename}")
     end
   end
 end
