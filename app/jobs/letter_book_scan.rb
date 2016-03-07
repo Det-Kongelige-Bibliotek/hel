7576450bbf1990d1a01eb4ab478ff36e85a4a327
David Grove Jørgensen <dgj@kb.dk>
2016-03-07 11:11:11 +0100

    some better logging

--- app/jobs/letter_book_scan.rb
+++ app/jobs/letter_book_scan.rb
@@ -5,9 +5,12 @@ class LetterBookScan
   def self.perform(incomming_path,processed_path,img_base_path)
     Dir.glob("#{incomming_path}/*.xml").each do |xml_path|
       basename = File.basename(xml_path).to_s
-      raise "File #{basename} is allready ingested" if ContentFile.find_by_original_filename(basename).present?
-      sysnum = basename.to_s.split('_')[0]
-      Resque.enqueue(LetterBookIngest,xml_path,"#{img_base_path}/#{File.basename(xml_path,'.xml')}","#{processed_path}/#{basename}")
+      unless ContentFile.find_by_original_filename(basename).present?
+        sysnum = basename.to_s.split('_')[0]
+        Resque.enqueue(LetterBookIngest,xml_path,"#{img_base_path}/#{File.basename(xml_path,'.xml')}","#{processed_path}/#{basename}")
+      else
+        Resque.logger.warn "#{basname} allready ingested. Skipping it"
+      end
     end
   end
 end
