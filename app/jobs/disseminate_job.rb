1c56116bb106ddf06fadd996704b0e3b02428685
David Grove Jørgensen <dgj@kb.dk>
2016-07-05 16:33:36 +0200

    more logging

--- app/jobs/disseminate_job.rb
+++ app/jobs/disseminate_job.rb
@@ -4,6 +4,7 @@ class DisseminateJob
   def self.perform(instance_id)
     i = Instance.where(id: instance_id).first
     raise "Instance #{instance_id} not found" unless i.present?
+    Resque.logger.debug "disseminating instance #{instance_id}"
     i.disseminate
   end
 
